a97d24219078934f4e117bbf7a0028d169d37c8f
apalzab <apalzab@gmail.com>
2013-12-19 16:03:55 +0100

    statics added

--- tidyourmind/urls.py
+++ tidyourmind/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, include, url, settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
