8b0eb313d390fc82b98918bbdf72595981992adb
apalzab <apalzab@gmail.com>
2013-12-19 15:52:06 +0100

    statics added

--- tidyourmind/settings.py
+++ tidyourmind/settings.py
@@ -72,7 +72,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    ('assets', '/Users/aitor/Documents/code/heroku/static'),
+    #('assets', '/Users/aitor/Documents/code/heroku/static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -187,3 +187,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+
+
+
