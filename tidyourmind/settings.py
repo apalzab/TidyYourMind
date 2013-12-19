8f08fcc2b27a170a657cd253dd1327b0ade0c752
apalzab <apalzab@gmail.com>
2013-12-19 20:56:14 +0100

    templates

--- tidyourmind/settings.py
+++ tidyourmind/settings.py
@@ -71,7 +71,7 @@ import os.path
 # Additional locations of static files
 PROJECT_DIR = os.path.dirname(__file__) # this is not Django setting.
 TEMPLATE_DIRS = (
-    os.path.join(PROJECT_DIR, "templates"),
+    os.path.join(PROJECT_DIR, "/templates/"),
     # here you can add another templates directory if you wish.
 )
 
